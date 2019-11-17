0d7966206a674caff5d1738b4211d6e82aec32c9
Tai Truong <giaptai170@gmail.com>
2019-11-17 11:05:03 +0700

    a

---
+++ app/src/main/java/com/ttt/gallery/MainActivity.java
@@ -0,0 +1,51 @@
+package com.ttt.gallery;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActionBar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = getSupportActionBar();
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.activity_main_bnv_nav);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        toolbar.setTitle("Ảnh");
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+            switch (item.getItemId()) {
+                case R.id.nav_img:
+                    toolbar.setTitle("Ảnh");
+                    fragment = new ImgFragment();
+                    return true;
+                case R.id.nav_album:
+                    toolbar.setTitle("Album");
+                    return true;
+                case R.id.nav_time:
+                    toolbar.setTitle("Time Line");
+                    return true;
+            }
+            return false;
+        }
+    };
+}
